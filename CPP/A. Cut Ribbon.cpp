81c5a50e1dbef80f61df57c812576a28c4bda55e
abdalla maher <abdallamaher89@gmail.com>
2021-11-03 17:59:05 +0200

    update

--- CPP/A. Cut Ribbon.cpp
+++ CPP/A. Cut Ribbon.cpp
@@ -7,66 +7,64 @@ https://codeforces.com/contest/189/problem/A
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int N = 4e3 + 5;
-int n, a, b, c;
-int dp[4][N];
-int mem[N];
 
-class Cut_Ribbon {
-public:
-    static void brute_force();
+int mem[(int)4e4 + 4];
 
-    static int Memoization(int rem);
+struct Cut_Ribbon {
+    int n, a, b, c;
+    vector<int>dp;
 
-    static void Table();
-};
+    Cut_Ribbon(int N, int A, int B, int C) {
+        n = N; a = A; b = B; c = C;
+        dp.resize(n * n);
+        memset(mem, -1, sizeof(mem));
+    }
 
-void Cut_Ribbon::brute_force(){
-    int ans = 0;
-    for (int i = 0; i <= n; i++) {
-        for (int ii = 0; ii <= n; ii++) {
-            int t = n - (a * i + b * ii);
-            if (t >= 0 && !(t % c)) {
-                ans = max(ans, i + ii + t / c);
+    int brute_force() {
+        int ans = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                int x = n - i * a - j * b;
+                if (x < 0 || x % c)continue;
+                ans = max(ans, i + j + x / c);
             }
         }
+        return ans;
     }
-    cout << ans;
-}
 
-int Cut_Ribbon::Memoization(int rem) {
-    if (rem == 0)return 0;
-    if (rem < 0)return -1e8;
-    if (~mem[rem])return mem[rem];
-    int ans = -1e9;
-    ans = max(ans, 1 + Memoization(rem - a));
-    ans = max(ans, 1 + Memoization(rem - b));
-    ans = max(ans, 1 + Memoization(rem - c));
-    return mem[rem] = ans;
-}
+    int Memoization(int rem) {
+        if (rem < 0)return -1e9;
+        if (rem == 0)return 0;
+        int& ret = mem[rem];
+        if (~ret)return ret;
+        int A = Memoization(rem - a) + 1;
+        int B = Memoization(rem - b) + 1;
+        int C = Memoization(rem - c) + 1;
+        return ret = max({ A, B, C });
+    }
 
-void Cut_Ribbon::Table() {
-    vector<int> v(n + 3);
-    v[1] = a, v[2] = b, v[3] = c;
-    for (int i = 1; i <= 3; i++) {
-        for (int j = 1; j <= n; j++) {
-            int a, b = 0;
-            a = dp[i - 1][j];
-            if (j >= v[i] && (j == v[i] || dp[i][j - v[i]])) b = dp[i][j - v[i]] + 1;
-            dp[i][j] = max(a, b);
+    int Table() {
+        for (int i = 1; i <= n;i++)dp[i] = -1;
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] == -1)continue;
+            dp[i + a] = max(dp[i + a], dp[i] + 1);
+            dp[i + b] = max(dp[i + b], dp[i] + 1);
+            dp[i + c] = max(dp[i + c], dp[i] + 1);
         }
+        return dp[n];
     }
-    cout << dp[3][n];
-}
+};
 
 int main() {
-    cin.sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n, a, b, c;
     cin >> n >> a >> b >> c;
-    memset(mem, -1, sizeof(mem));
-    Cut_Ribbon::Table();
-    //cout << Cut_Ribbon::Memoization(n);
-    //Cut_Ribbon::brute_force();
+    Cut_Ribbon* e;
+    e = new Cut_Ribbon(n, a, b, c);
+    cout << e->Table();
+    // cout << e->Memoization(n);
+    // cout << e->brute_force();
     return 0;
 }
+
 
